c548159df1053086734366444278b1dcc10bc887
leonardo.coutinho <leonardo.coutinho@ubc.org.br>
2021-04-29 14:41:26 -0300

    Add project files.

---
+++ TesteConexaoOracle/Conexao.cs
@@ -0,0 +1,71 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Data;
+
+namespace TesteConexaoOracle
+{
+    public class Conexao
+    {
+        public Conexao()
+        {
+           
+        }
+       
+        public static OracleConnection Conn { get; set; }
+        
+        public static void Conectar()
+        {
+            string uid = "adaweb";
+            string passwd = "arquivox";
+            string host = "192.168.0.202";
+            int port1 = 1521;
+            int port2 = 1526;
+            string service = "webdsv";
+
+            if (Conn != null)
+            {
+                if (Conn.State != ConnectionState.Closed)
+                {
+                    Conn.Close();
+                    Conn.Dispose();
+                }
+            }
+
+            string dataSource = string.Format(
+                "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={0})(PORT={1}))(CONNECT_DATA=(SERVICE_NAME={2})))",
+                host,
+                port1,
+                service);
+
+            try
+            {
+                var ocsb = new OracleConnectionStringBuilder()
+                {
+                    UserID = uid,
+                    Password = passwd,
+                    DataSource = dataSource
+                };
+
+                Conn = new OracleConnection(ocsb.ConnectionString);
+                Conn.Open();
+            }
+            catch (Exception ex)
+            {               
+              throw new Exception(ex.Message);
+                
+            }
+        }
+
+        /// <summary>
+        /// Fechar conexão.
+        /// </summary>
+        public static void FecharConexao()
+        {
+            if (Conn != null)
+            {
+                Conn.Close();
+                Conn.Dispose();
+            }
+        }
+    }
+}
---
+++ TesteConexaoOracle/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TesteConexaoOracle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listando um titular");
+            Console.WriteLine("");
+
+            GetTitular(19045900);
+
+            Console.ReadKey();
+        }
+
+
+        public static void GetTitular(long cdTitular)
+        {
+            try
+            {
+                //chamando a conexão
+                Conexao.Conectar();
+                var con = Conexao.Conn;
+
+                using (var comm = new Oracle.ManagedDataAccess.Client.OracleCommand())
+                {
+                    comm.Connection = con;
+                    comm.CommandText = $"SELECT CD_TITULAR,NM_TITULAR,NM_CIDADE FROM ADAWEB.TITULAR T WHERE T.CD_TITULAR = {cdTitular}";
+
+                    using (var reader = comm.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("CÓDIGO UBC: {0}", reader["CD_TITULAR"]);
+                            Console.WriteLine("NOME COMPLETO: {0}", reader["NM_TITULAR"]);
+                            Console.WriteLine("E-MAIL: {0}", reader["NM_CIDADE"]);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                Conexao.FecharConexao();
+            }
+
+        }
+    }
+}
